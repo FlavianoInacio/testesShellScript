95379cbb23c2b64e552f61318b974dd16fad5a47
Flaviano Inácio <flaviano.inacio@prodeb.ba.gov.br>
2021-03-12 08:50:53 -0300

    testes de arumentos

---
+++ argumentos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#entedimento sobre argumentos
+
+echo "Primeiro argumento $1"
+echo "Segundo argumento $2"
+
+echo "Foram passados um total de $# argumentos"
+echo "Os argumentos passados foram $@"
